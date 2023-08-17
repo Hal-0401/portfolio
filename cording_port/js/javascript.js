b466aa752f882a1bafda9112631dac5f5133969f
Hal-0401 <h_wada_0429@yahoo.co.jp>
2023-08-17 14:33:48 +0900

    cording

--- cording_port/js/javascript.js
+++ cording_port/js/javascript.js
@@ -1,17 +1,17 @@
 'use strict'
-
+window.addEventListener('load', function(){
 window.addEventListener("scroll", function () {
   // ヘッダーを変数の中に格納する
   const header = document.querySelector("header");
   // 100px以上スクロールしたらヘッダーに「scroll-nav」クラスをつける
-  header.classList.toggle("scroll-nav", window.scrollY > 100);
+  header.classList.toggle("scroll-nav", window.scrollY > 80);
 });
 
 
 document.querySelector('.menu-btn').addEventListener('click', function(){
     this.classList.toggle('active');
     document.querySelector('.sub-nav').classList.toggle('active');
-  })
+  });
 
   
   const targets = document.getElementsByClassName('fade');
@@ -26,3 +26,12 @@ for(let i = targets.length; i--;){
  });
  observer.observe(targets[i]);
 }
+});
+
+window.addEventListener("scroll", function () {
+  const topBtn = document.getElementById("top-btn");
+  const scroll = window.scrollY;
+  if (scroll > 700) { 
+    topBtn.style.opacity = 1;
+  } else topBtn.style.opacity = 0; 
+});
